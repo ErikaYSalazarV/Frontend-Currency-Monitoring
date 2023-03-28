0705e1de6a8347407e03cd2db10456437ebc06c0
Carlos Orduz <Carlos.Orduz@endava.com>
2023-03-27 22:38:08 -0500

    merge coins and stocks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import CoinMarketCap from './Components/CoinmarketCap/CoinMarketCap';
 import Stocks from './Components/CoinmarketCap/Stocks';
 import "./CSS/CoinMarketCap.css"
 function App() {
   return (
     <div>
+      <CoinMarketCap/>
       <Stocks />
+      
     </div>
   );
 }
